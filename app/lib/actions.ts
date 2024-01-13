b0f1f205a008c01263b9cf3532b40c6b06081b0c
Uğur Kağan Çakır <80484731+ukcx@users.noreply.github.com>
2024-01-13 23:45:56 +0300

    reverted

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -74,5 +74,7 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
     return { message: 'Deleted Invoice.' };
-  } 
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
 }
